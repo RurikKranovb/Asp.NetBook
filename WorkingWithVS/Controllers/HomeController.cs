866540b4bf6c94f0a621eff3f66eee76c54420a2
bobrov.da <dmr.bobrov@yandex.ru>
2025-05-20 21:14:42 +0300

    Refactoring

--- WorkingWithVS/Controllers/HomeController.cs
+++ WorkingWithVS/Controllers/HomeController.cs
@@ -5,7 +5,19 @@ namespace WorkingWithVS.Controllers
 {
     public class HomeController : Controller
     {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+
         public IActionResult Index() => View(SimpleRepository.SharedRepository.Products
             .Where(p => p?.Price <50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            Repository.AddProduct(product);
+            return RedirectToAction("Index");
+        }
     }
 }
