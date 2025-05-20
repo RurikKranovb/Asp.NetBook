32dbfd8089d8bc02a00dd95db7559aeb71d051e7
bobrov.da <dmr.bobrov@yandex.ru>
2025-05-20 21:14:47 +0300

    Add test project

---
+++ WorkingWithVS.Tests/ProductTests.cs
@@ -0,0 +1,29 @@
+﻿using WorkingWithVS.Models;
+
+namespace WorkingWithVS.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            //Организация
+            var p = new Product() { Name = "Test", Price = 100M };
+            //Действие
+            p.Name = "New Name";
+            //Утверждение
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            //Организация
+            var p = new Product() { Name = "Test", Price = 100M };
+            //Действие
+            p.Price = 200M;
+            //Утверждение
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
