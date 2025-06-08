f2ffbb95c061ed0bf87fa00e44bb64960d4526c0
bobrov.da <dmr.bobrov@yandex.ru>
2025-06-08 05:52:51 +0300

    Refactoring

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -15,6 +15,24 @@ namespace SportsStore.Controllers
             _cart = cart;
         }
 
+        public ViewResult List() =>
+            View(_repository.Orders.Where(o => !o.Shipped));
+
+        [HttpPost]
+        public IActionResult MarkShipped(int orderId)
+        {
+            Order order = _repository.Orders
+                .FirstOrDefault(o => o.OrderId == orderId);
+
+            if (order != null)
+            {
+                order.Shipped = true;
+                _repository.SaveOrder(order);
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
