298970b1d4e3a78582b8a2d4f288f686c5d84fa1
bobrov.da <dmr.bobrov@yandex.ru>
2025-06-08 05:53:16 +0300

    Add Admin controller/view. TestAdminController not work

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index() => View(_repository.Products);
+    }
+}
