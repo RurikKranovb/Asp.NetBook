cf7e462ae91ddfc19a8ff92a099831af22651f99
bobrov.da <dmr.bobrov@yandex.ru>
2025-07-02 20:40:30 +0300

    Add blank

---
+++ UrlsAndRoutes/Models/Result.cs
@@ -0,0 +1,10 @@
+﻿namespace UrlsAndRoutes.Models
+{
+    public class Result
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public IDictionary<string, object> Data { get; } = new Dictionary<string, object>();
+
+    }
+}
--- UrlsAndRoutes/Program.cs
+++ UrlsAndRoutes/Program.cs
@@ -14,7 +14,11 @@ namespace UrlsAndRoutes
             app.UseStaticFiles();
             app.MapRazorPages();
 
-            app.MapGet("/", () => "Hello World!");
+
+            app.MapControllerRoute(
+
+                name: "default",
+                pattern: "{controller}/{action=Index}");
 
             app.Run();
         }
