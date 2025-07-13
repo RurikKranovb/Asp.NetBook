f6285eb095fe2bb092954860b83d09d45943786a
bobrov.da <dmr.bobrov@yandex.ru>
2025-07-13 21:49:29 +0300

    Заготовка

---
+++ DependencyInjection/DependencyInjection/Infrastructure/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using DependencyInjection.Models;
+
+namespace DependencyInjection.Infrastructure
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        Product this[string name] { get; }
+
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+    }
+}
---
+++ DependencyInjection/DependencyInjection/Models/MemoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using DependencyInjection.Infrastructure;
+
+namespace DependencyInjection.Models
+{
+    public class MemoryRepository : IRepository
+    {
+        private Dictionary<string, Product> _products;
+
+        public MemoryRepository()
+        {
+            _products = new Dictionary<string, Product>();
+
+            new List<Product>()
+            {
+                new Product{Name = "Kayak", Price = 175M},
+                new Product{Name = "Lifejacket", Price = 48.95M},
+                new Product{Name = "Soccer ball", Price = 19.5M},
+            }.ForEach(p => AddProduct(p));
+        }
+
+        public IEnumerable<Product> Products => _products.Values;
+        public Product this[string name] => _products[name];
+
+        public void AddProduct(Product product)
+        {
+            _products[product.Name] = product;
+        }
+
+        public void DeleteProduct(Product product) => _products.Remove(product.Name);
+    }
+}
