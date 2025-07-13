538ba47fe557229ae59e52d3f6cf89ae7c8ce1c6
bobrov.da <dmr.bobrov@yandex.ru>
2025-07-13 21:29:55 +0300

    End

---
+++ ControllersAndActions/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);
+        public RedirectResult RedirectResult() => Redirect("/Example/Index");
+    }
+}
