41e41cd838a74cf8702e20e94c64014c66db6bd6
bobrov.da <dmr.bobrov@yandex.ru>
2025-07-22 19:40:25 +0300

    Filter

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Filters.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filters.Controllers
+{
+
+    [HttpsOnly]
+    public class HomeController : Controller
+    {
+
+        //[RequireHttps]
+        public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
+
+
+        //public IActionResult Index()
+        //{
+        //    if (!Request.IsHttps)
+        //    {
+        //        return new StatusCodeResult(StatusCodes.Status403Forbidden);
+        //    }
+        //    else
+        //    {
+        //        return View("Message", "This is the Index action on the Home controller");
+        //    }
+        //}
+        //public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
+    }
+}
---
+++ Filters/Filters/Program.cs
@@ -0,0 +1,48 @@
+namespace Filters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddMemoryCache();
+            builder.Services.AddSession();
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.UseEndpoints(endpoints =>
+            {
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}"
+                );
+            });
+
+            app.Run();
+        }
+    }
+}
